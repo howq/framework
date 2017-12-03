156ae27bfcf72d7b6e4b781717c4107e4b768a6e
howq <howq@outlook.com>
2017-12-04 00:57:24 +0800

    fix(db): add db comment to fields

--- wordpress-api/doc/sql/wp_bm.sql
+++ wordpress-api/doc/sql/wp_bm.sql
@@ -20,10 +20,10 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `wp_commentmeta`;
 CREATE TABLE `wp_commentmeta` (
-  `meta_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `comment_id` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `meta_key` varchar(255) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
-  `meta_value` longtext COLLATE utf8mb4_unicode_520_ci,
+  `meta_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增唯一ID',
+  `comment_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '对应评论ID',
+  `meta_key` varchar(255) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL COMMENT '键名',
+  `meta_value` longtext COLLATE utf8mb4_unicode_520_ci COMMENT '键值',
   PRIMARY KEY (`meta_id`),
   KEY `comment_id` (`comment_id`),
   KEY `meta_key` (`meta_key`(191))
@@ -34,21 +34,21 @@ CREATE TABLE `wp_commentmeta` (
 -- ----------------------------
 DROP TABLE IF EXISTS `wp_comments`;
 CREATE TABLE `wp_comments` (
-  `comment_ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `comment_post_ID` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `comment_author` tinytext COLLATE utf8mb4_unicode_520_ci NOT NULL,
-  `comment_author_email` varchar(100) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
-  `comment_author_url` varchar(200) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
-  `comment_author_IP` varchar(100) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
-  `comment_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `comment_date_gmt` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `comment_content` text COLLATE utf8mb4_unicode_520_ci NOT NULL,
-  `comment_karma` int(11) NOT NULL DEFAULT '0',
-  `comment_approved` varchar(20) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '1',
-  `comment_agent` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
-  `comment_type` varchar(20) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
-  `comment_parent` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `user_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `comment_ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增唯一ID',
+  `comment_post_ID` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '对应文章ID',
+  `comment_author` tinytext COLLATE utf8mb4_unicode_520_ci NOT NULL COMMENT '评论者',
+  `comment_author_email` varchar(100) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '' COMMENT '评论者邮箱',
+  `comment_author_url` varchar(200) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '' COMMENT '评论者网址',
+  `comment_author_IP` varchar(100) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '' COMMENT '评论者IP',
+  `comment_date` datetime NOT NULL DEFAULT '1000-01-01 00:00:00' COMMENT '评论时间',
+  `comment_date_gmt` datetime NOT NULL DEFAULT '1000-01-01 00:00:00' COMMENT '评论时间（GMT+0时间）',
+  `comment_content` text COLLATE utf8mb4_unicode_520_ci NOT NULL COMMENT '评论正文',
+  `comment_karma` int(11) NOT NULL DEFAULT '0' COMMENT '',
+  `comment_approved` varchar(20) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '1' COMMENT '评论是否被批准',
+  `comment_agent` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '' COMMENT '评论者的USER AGENT',
+  `comment_type` varchar(20) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '' COMMENT '评论类型(pingback/普通)',
+  `comment_parent` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '父评论ID',
+  `user_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '评论者用户ID（不一定存在）',
   PRIMARY KEY (`comment_ID`),
   KEY `comment_post_ID` (`comment_post_ID`),
   KEY `comment_approved_date_gmt` (`comment_approved`,`comment_date_gmt`),
@@ -62,19 +62,19 @@ CREATE TABLE `wp_comments` (
 -- ----------------------------
 DROP TABLE IF EXISTS `wp_links`;
 CREATE TABLE `wp_links` (
-  `link_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `link_url` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
-  `link_name` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
-  `link_image` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
-  `link_target` varchar(25) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
-  `link_description` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
-  `link_visible` varchar(20) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT 'Y',
-  `link_owner` bigint(20) unsigned NOT NULL DEFAULT '1',
-  `link_rating` int(11) NOT NULL DEFAULT '0',
-  `link_updated` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `link_rel` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
-  `link_notes` mediumtext COLLATE utf8mb4_unicode_520_ci NOT NULL,
-  `link_rss` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
+  `link_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增唯一ID',
+  `link_url` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '' COMMENT '链接URL',
+  `link_name` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '' COMMENT '链接标题',
+  `link_image` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '' COMMENT '链接图片',
+  `link_target` varchar(25) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '' COMMENT '链接打开方式',
+  `link_description` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '' COMMENT '链接描述',
+  `link_visible` varchar(20) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT 'Y' COMMENT '是否可见（Y/N）',
+  `link_owner` bigint(20) unsigned NOT NULL DEFAULT '1' COMMENT '添加者用户ID',
+  `link_rating` int(11) NOT NULL DEFAULT '0' COMMENT '评分等级',
+  `link_updated` datetime NOT NULL DEFAULT '1000-01-01 00:00:00' COMMENT '',
+  `link_rel` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '' COMMENT 'XFN关系',
+  `link_notes` mediumtext COLLATE utf8mb4_unicode_520_ci NOT NULL COMMENT 'XFN注释',
+  `link_rss` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '' COMMENT '链接RSS地址',
   PRIMARY KEY (`link_id`),
   KEY `link_visible` (`link_visible`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
@@ -84,10 +84,10 @@ CREATE TABLE `wp_links` (
 -- ----------------------------
 DROP TABLE IF EXISTS `wp_options`;
 CREATE TABLE `wp_options` (
-  `option_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `option_name` varchar(191) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
-  `option_value` longtext COLLATE utf8mb4_unicode_520_ci NOT NULL,
-  `autoload` varchar(20) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT 'yes',
+  `option_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增唯一ID',
+  `option_name` varchar(191) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '' COMMENT '键名',
+  `option_value` longtext COLLATE utf8mb4_unicode_520_ci NOT NULL COMMENT '键值',
+  `autoload` varchar(20) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT 'yes' COMMENT '在WordPress载入时自动载入（yes/no）',
   PRIMARY KEY (`option_id`),
   UNIQUE KEY `option_name` (`option_name`)
 ) ENGINE=MyISAM AUTO_INCREMENT=240 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
@@ -97,10 +97,10 @@ CREATE TABLE `wp_options` (
 -- ----------------------------
 DROP TABLE IF EXISTS `wp_postmeta`;
 CREATE TABLE `wp_postmeta` (
-  `meta_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `post_id` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `meta_key` varchar(255) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
-  `meta_value` longtext COLLATE utf8mb4_unicode_520_ci,
+  `meta_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增唯一ID',
+  `post_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '对应文章ID',
+  `meta_key` varchar(255) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL COMMENT '键名',
+  `meta_value` longtext COLLATE utf8mb4_unicode_520_ci COMMENT '键值',
   PRIMARY KEY (`meta_id`),
   KEY `post_id` (`post_id`),
   KEY `meta_key` (`meta_key`(191))
@@ -111,29 +111,29 @@ CREATE TABLE `wp_postmeta` (
 -- ----------------------------
 DROP TABLE IF EXISTS `wp_posts`;
 CREATE TABLE `wp_posts` (
-  `ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `post_author` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `post_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `post_date_gmt` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `post_content` longtext COLLATE utf8mb4_unicode_520_ci NOT NULL,
-  `post_title` text COLLATE utf8mb4_unicode_520_ci NOT NULL,
-  `post_excerpt` text COLLATE utf8mb4_unicode_520_ci NOT NULL,
-  `post_status` varchar(20) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT 'publish',
-  `comment_status` varchar(20) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT 'open',
-  `ping_status` varchar(20) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT 'open',
-  `post_password` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
-  `post_name` varchar(200) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
-  `to_ping` text COLLATE utf8mb4_unicode_520_ci NOT NULL,
-  `pinged` text COLLATE utf8mb4_unicode_520_ci NOT NULL,
-  `post_modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `post_modified_gmt` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `post_content_filtered` longtext COLLATE utf8mb4_unicode_520_ci NOT NULL,
-  `post_parent` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `guid` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
-  `menu_order` int(11) NOT NULL DEFAULT '0',
-  `post_type` varchar(20) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT 'post',
-  `post_mime_type` varchar(100) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
-  `comment_count` bigint(20) NOT NULL DEFAULT '0',
+  `ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增唯一ID',
+  `post_author` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '对应作者ID',
+  `post_date` datetime NOT NULL DEFAULT '1000-01-01 00:00:00' COMMENT '发布时间',
+  `post_date_gmt` datetime NOT NULL DEFAULT '1000-01-01 00:00:00' COMMENT '发布时间（GMT+0时间）',
+  `post_content` longtext COLLATE utf8mb4_unicode_520_ci NOT NULL COMMENT '正文',
+  `post_title` text COLLATE utf8mb4_unicode_520_ci NOT NULL COMMENT '标题',
+  `post_excerpt` text COLLATE utf8mb4_unicode_520_ci NOT NULL COMMENT '摘录',
+  `post_status` varchar(20) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT 'publish' COMMENT '文章状态（publish/auto-draft/inherit等）',
+  `comment_status` varchar(20) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT 'open' COMMENT '评论状态（open/closed）',
+  `ping_status` varchar(20) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT 'open' COMMENT 'PING状态（open/closed）',
+  `post_password` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '' COMMENT '文章密码',
+  `post_name` varchar(200) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '' COMMENT '文章缩略名',
+  `to_ping` text COLLATE utf8mb4_unicode_520_ci NOT NULL COMMENT '',
+  `pinged` text COLLATE utf8mb4_unicode_520_ci NOT NULL COMMENT '已经PING过的链接',
+  `post_modified` datetime NOT NULL DEFAULT '1000-01-01 00:00:00' COMMENT '修改时间',
+  `post_modified_gmt` datetime NOT NULL DEFAULT '1000-01-01 00:00:00' COMMENT '修改时间（GMT+0时间）',
+  `post_content_filtered` longtext COLLATE utf8mb4_unicode_520_ci NOT NULL COMMENT '',
+  `post_parent` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '父文章，主要用于PAGE',
+  `guid` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '' COMMENT '',
+  `menu_order` int(11) NOT NULL DEFAULT '0' COMMENT '排序ID',
+  `post_type` varchar(20) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT 'post' COMMENT '文章类型（post/page等）',
+  `post_mime_type` varchar(100) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '' COMMENT 'MIME类型',
+  `comment_count` bigint(20) NOT NULL DEFAULT '0' COMMENT '评论总数',
   PRIMARY KEY (`ID`),
   KEY `post_name` (`post_name`(191)),
   KEY `type_status_date` (`post_type`,`post_status`,`post_date`,`ID`),
@@ -146,10 +146,10 @@ CREATE TABLE `wp_posts` (
 -- ----------------------------
 DROP TABLE IF EXISTS `wp_termmeta`;
 CREATE TABLE `wp_termmeta` (
-  `meta_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `term_id` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `meta_key` varchar(255) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
-  `meta_value` longtext COLLATE utf8mb4_unicode_520_ci,
+  `meta_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增唯一ID',
+  `term_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '对应分类ID',
+  `meta_key` varchar(255) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL COMMENT '键名',
+  `meta_value` longtext COLLATE utf8mb4_unicode_520_ci COMMENT '键值',
   PRIMARY KEY (`meta_id`),
   KEY `term_id` (`term_id`),
   KEY `meta_key` (`meta_key`(191))
@@ -160,10 +160,10 @@ CREATE TABLE `wp_termmeta` (
 -- ----------------------------
 DROP TABLE IF EXISTS `wp_terms`;
 CREATE TABLE `wp_terms` (
-  `term_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(200) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
-  `slug` varchar(200) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
-  `term_group` bigint(10) NOT NULL DEFAULT '0',
+  `term_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '分类ID',
+  `name` varchar(200) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '' COMMENT '分类名',
+  `slug` varchar(200) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '' COMMENT '缩略名',
+  `term_group` bigint(10) NOT NULL DEFAULT '0' COMMENT '分类组别',
   PRIMARY KEY (`term_id`),
   KEY `slug` (`slug`(191)),
   KEY `name` (`name`(191))
@@ -174,9 +174,9 @@ CREATE TABLE `wp_terms` (
 -- ----------------------------
 DROP TABLE IF EXISTS `wp_term_relationships`;
 CREATE TABLE `wp_term_relationships` (
-  `object_id` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `term_taxonomy_id` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `term_order` int(11) NOT NULL DEFAULT '0',
+  `object_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '对应文章ID/链接ID',
+  `term_taxonomy_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '对应分类方法ID',
+  `term_order` int(11) NOT NULL DEFAULT '0' COMMENT '排序',
   PRIMARY KEY (`object_id`,`term_taxonomy_id`),
   KEY `term_taxonomy_id` (`term_taxonomy_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
@@ -186,12 +186,12 @@ CREATE TABLE `wp_term_relationships` (
 -- ----------------------------
 DROP TABLE IF EXISTS `wp_term_taxonomy`;
 CREATE TABLE `wp_term_taxonomy` (
-  `term_taxonomy_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `term_id` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `taxonomy` varchar(32) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
-  `description` longtext COLLATE utf8mb4_unicode_520_ci NOT NULL,
-  `parent` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `count` bigint(20) NOT NULL DEFAULT '0',
+  `term_taxonomy_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '分类方法ID',
+  `term_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '分类方法(category/post_tag)',
+  `taxonomy` varchar(32) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '' COMMENT '描述',
+  `description` longtext COLLATE utf8mb4_unicode_520_ci NOT NULL COMMENT '排序',
+  `parent` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '所属父分类方法ID',
+  `count` bigint(20) NOT NULL DEFAULT '0' COMMENT '文章数统计',
   PRIMARY KEY (`term_taxonomy_id`),
   UNIQUE KEY `term_id_taxonomy` (`term_id`,`taxonomy`),
   KEY `taxonomy` (`taxonomy`)
@@ -202,10 +202,10 @@ CREATE TABLE `wp_term_taxonomy` (
 -- ----------------------------
 DROP TABLE IF EXISTS `wp_usermeta`;
 CREATE TABLE `wp_usermeta` (
-  `umeta_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `meta_key` varchar(255) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
-  `meta_value` longtext COLLATE utf8mb4_unicode_520_ci,
+  `umeta_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增唯一ID',
+  `user_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '对应用户ID',
+  `meta_key` varchar(255) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL COMMENT '键名',
+  `meta_value` longtext COLLATE utf8mb4_unicode_520_ci COMMENT '键值',
   PRIMARY KEY (`umeta_id`),
   KEY `user_id` (`user_id`),
   KEY `meta_key` (`meta_key`(191))
@@ -216,16 +216,16 @@ CREATE TABLE `wp_usermeta` (
 -- ----------------------------
 DROP TABLE IF EXISTS `wp_users`;
 CREATE TABLE `wp_users` (
-  `ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `user_login` varchar(60) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
-  `user_pass` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
-  `user_nicename` varchar(50) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
-  `user_email` varchar(100) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
-  `user_url` varchar(100) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
-  `user_registered` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `user_activation_key` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
-  `user_status` int(11) NOT NULL DEFAULT '0',
-  `display_name` varchar(250) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
+  `ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增唯一ID',
+  `user_login` varchar(60) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '' COMMENT '登录名',
+  `user_pass` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '' COMMENT '密码',
+  `user_nicename` varchar(50) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '' COMMENT '昵称',
+  `user_email` varchar(100) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '' COMMENT 'Email',
+  `user_url` varchar(100) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '' COMMENT '网址',
+  `user_registered` datetime NOT NULL DEFAULT '1000-01-01 00:00:00' COMMENT '注册时间',
+  `user_activation_key` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '' COMMENT '激活码',
+  `user_status` int(11) NOT NULL DEFAULT '0' COMMENT '用户状态',
+  `display_name` varchar(250) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '' COMMENT '用户显示名称',
   PRIMARY KEY (`ID`),
   KEY `user_login_key` (`user_login`),
   KEY `user_nicename` (`user_nicename`),
